5d752451b18bbdd83d043174f44573372fe32787
Pranjal_pandey98 <pranjal.pandey@nagarro.com>
2023-04-03 20:36:27 +0530

    build location change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -9,25 +9,25 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   // root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../build',
-  },
+  // build: {
+  //   outDir: '../build',
+  // },
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom', // add jsdom
-    setupFiles: './src/tests/setupTests.ts', // setup includes
+    environment: "jsdom", // add jsdom
+    setupFiles: "./src/tests/setupTests.ts", // setup includes
     coverage: {
       // config coverage
-      reporter: ['text', 'html'], // generate coverage reports
+      reporter: ["text", "html"], // generate coverage reports
       exclude: [
         // dont test the following files
-        'node_modules/',
-        'src/tests/setupTests.ts',
+        "node_modules/",
+        "src/tests/setupTests.ts",
       ],
     },
   },
